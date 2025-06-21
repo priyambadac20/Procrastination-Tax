108e1236732a4b7b309779e8a5c14453748ae7f6
Priyambada Chouhan <priyambadachouhan@gmail.com>
2025-06-21 10:16:57 +0530

    Update Procastination_Tax.sol

--- Contracts/Procastination_Tax.sol
+++ Contracts/Procastination_Tax.sol
@@ -11,11 +11,6 @@ abstract contract Context {
     }
 }
 
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- */
 abstract contract Ownable is Context {
     address private _owner;
 
